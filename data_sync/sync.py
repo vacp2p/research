fad3987e8014ede0d61441310207f27c883df5ee
Oskar Thoren <ot@oskarthoren.com>
2019-01-09 08:03:37 -0500

    Introduce random uniform naive latency

--- data_sync/sync.py
+++ data_sync/sync.py
@@ -53,13 +53,18 @@ class NetworkSimulator():
             n.tick()
         self.time += 1
 
+    # XXX: This should be normal distribution or Poisson
+    def latency_uniform_random(self):
+        latency = random.randint(1,3)
+        return latency
+
     # NOTE: Direct message, no broadcast etc
     def send_message(self, sender_id, receiver_id, message):
         #print "*** (network) send_message", sender_id, receiver_id
         # XXX: Assuming sender exists
         sender = self.peers[sender_id]
         receiver = self.peers[receiver_id]
-        recv_time = self.time + 1
+        recv_time = self.time + self.latency_uniform_random()
         if recv_time not in self.queue:
             self.queue[recv_time] = []
         self.queue[recv_time].append((sender, receiver, message))
