5cbb5eda1506062688ddaf7298a91b701a0a8a61
Oskar Thoren <ot@oskarthoren.com>
2019-02-21 01:36:48 -0500

    switch a and b node

--- data_sync/app.py
+++ data_sync/app.py
@@ -1,11 +1,18 @@
-import networkwhisper, sync, threading, time
+import networkwhisper, sync, sys, threading, time
 
 # XXX: Ugly constants, should be elsewhere
-# TODO: Should be symmetric for a and b
-# TODO: Read this in from environment
-A_KEYPAIR = "0x57083392b29bdf24512c93cfdf45d38c87d9d882da3918c59f4406445ea976a4"
-A_PUBKEY = "0x04d94a1a01872b598c7cdc5aca2358d35eb91cd8a91eaea8da277451bb71d45c0d1eb87a31ea04e32f537e90165c870b3e115a12438c754d507ac75bddd6ecacd5"
-B_PUBKEY = "0x04ff921ddf78b5ed4537402f59a150caf9d96a83f2a345a1ddf9df12e99e7778f314c9ca72e8285eb213af84f5a7b01aabb62c67e46657976ded6658e1b9e83c73"
+SETTINGS = {
+    'a': {
+        'keypair': "0x57083392b29bdf24512c93cfdf45d38c87d9d882da3918c59f4406445ea976a4",
+        'pubkey': "0x04d94a1a01872b598c7cdc5aca2358d35eb91cd8a91eaea8da277451bb71d45c0d1eb87a31ea04e32f537e90165c870b3e115a12438c754d507ac75bddd6ecacd5",
+        'friend' : "0x04ff921ddf78b5ed4537402f59a150caf9d96a83f2a345a1ddf9df12e99e7778f314c9ca72e8285eb213af84f5a7b01aabb62c67e46657976ded6658e1b9e83c73" #b
+        },
+    'b': {
+        'keypair': "0x7b5c5af9736d9f1773f2020dd0fef0bc3c8aeaf147d2bf41961e766588e086e7",
+        'pubkey' : "0x04ff921ddf78b5ed4537402f59a150caf9d96a83f2a345a1ddf9df12e99e7778f314c9ca72e8285eb213af84f5a7b01aabb62c67e46657976ded6658e1b9e83c73",
+        'friend': "0x04d94a1a01872b598c7cdc5aca2358d35eb91cd8a91eaea8da277451bb71d45c0d1eb87a31ea04e32f537e90165c870b3e115a12438c754d507ac75bddd6ecacd5" #a
+    }
+}
 
 def tick_process(node):
     while True:
@@ -14,19 +21,23 @@ def tick_process(node):
         time.sleep(1)
 
 def main():
-    # Lets check lunch a bit
-    # Then, we can actually append message, and if we recv something print it
-    # Then make callable so python app.py <json file or smt>
+
+    assert len(sys.argv) > 1, "Missing node argument. Example: 'a' or 'b'"
+    # Assume exists
+    settings = SETTINGS[sys.argv[1]]
+    keypair = settings['keypair']
+    identity_pk = settings['pubkey']
+    friend_pk = settings['friend']
 
     # Init node
-    whisper_node = networkwhisper.WhisperNodeHelper(A_KEYPAIR)
-    node = sync.Node(A_PUBKEY, whisper_node, 'burstyMobile', 'batch')
+    whisper_node = networkwhisper.WhisperNodeHelper(keypair)
+    node = sync.Node(identity_pk, whisper_node, 'burstyMobile', 'batch')
     # XXX: A bit weird? Or very weird
     node.nodes = [node]
     # XXX: Doesn't make sense, a doesn't have b info
-    #node.addPeer(B_PUBKEY, b)
+    #node.addPeer(friend_pk, b)
     # Clients should decide policy
-    node.share(B_PUBKEY)
+    node.share(friend_pk)
 
     # Start background thread
     thread = threading.Thread(target=tick_process, args=[node])
