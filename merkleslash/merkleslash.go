49cbb2ee4165671888de643e0104dbf0e6f7c924
Oskar Thoren <ot@oskarthoren.com>
2019-04-16 13:50:30 +0800

    Merkleslash init

---
+++ merkleslash/merkleslash.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// Rough API
+
+// eg swarm feeds, etc - any trusted point
+// getRoot(method, groupID)
+
+// list or merkle root and payment
+// to untrusted node
+// sync(root, [have], node) payment? 
+
+//verify 
+//slash
+//court
+
+// define how untrusted nodes get in first place
+
+func main() {
+	fmt.Printf("Hello Merkleslash\n")
+}
