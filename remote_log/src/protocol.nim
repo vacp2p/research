f9745505f01ee59f4b29985e386ae30fd1c47ffc
Oskar Thoren <ot@oskarthoren.com>
2019-08-12 00:44:18 +0800

    Add CASResponse type

--- remote_log/src/protocol.nim
+++ remote_log/src/protocol.nim
@@ -15,27 +15,6 @@ message ExampleMessage {
 
 // Note that some of these are for mocking and might not be part of the spec.
 
-// Assuming only one
-message CASPostRequest {
-  string data = 1;
-}
-
-// Respond with hash for success or error
-// TODO: Maybe respond with more
-message CASPostResponse {
-  string id = 1;
-  string data = 2;
-}
-
-message CASGetRequest {
-  string id = 1;
-}
-
-message CASGetResponse {
-  string id = 1;
-  string data = 2;
-}
-
 // TODO: This needs more fleshing out, what data is
 message NSPostRequest {
   string data = 1;
@@ -75,6 +54,13 @@ message CASRequest {
   string id = 2;
   string data = 3;
 }
+
+// XXX: Differentiate between post and get reply?
+message CASResponse {
+  string id = 1;
+  string data = 2;
+}
+
 """
 parseProto(protoSpec)
 
