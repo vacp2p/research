e95a998375b2ca221be2a40aa09df167290fe7b9
Oskar Thoren <ot@oskarthoren.com>
2019-11-22 15:12:10 +0800

    hello-abnf instaparse init

---
+++ abnf/hello-abnf/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hello-abnf
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ abnf/hello-abnf/test/hello_abnf/core_test.clj
@@ -0,0 +1,7 @@
+(ns hello-abnf.core-test
+  (:require [clojure.test :refer :all]
+            [hello-abnf.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
