17c5d87462c7d3262f50b21b60d3e3367cb74cfc
Oskar Thoren <ot@oskarthoren.com>
2019-11-22 15:15:50 +0800

    instaparse hello world

--- abnf/hello-abnf/src/hello_abnf/core.clj
+++ abnf/hello-abnf/src/hello_abnf/core.clj
@@ -1,6 +1,16 @@
-(ns hello-abnf.core)
+(ns hello-abnf.core
+  (:require [instaparse.core :as insta]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(def as-and-bs
+  (insta/parser
+   "S = AB*
+     AB = A B
+     A = 'a'+
+     B = 'b'+"))
+
+(as-and-bs "aaaaabbbaaaabb")
