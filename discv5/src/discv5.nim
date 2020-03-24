f1259d99909f19fdf0bf20c08abf7fe11d774516
decanus <7621705+decanus@users.noreply.github.com>
2020-03-24 21:32:04 +0100

    experimenting

--- discv5/src/discv5.nim
+++ discv5/src/discv5.nim
@@ -1,16 +1,20 @@
 import
   random, chronos, sequtils, chronicles, tables, stint, options,
-  eth/[keys, rlp], eth/p2p/enode, eth/trie/db,
+  eth/[keys, rlp, async_utils], eth/p2p/enode, eth/trie/db,
   eth/p2p/discoveryv5/[discovery_db, enr, node, types, routing_table, encoding],
   eth/p2p/discoveryv5/protocol as discv5_protocol,
   ./utils
 
-proc run() =
+proc run() {.async.} =
     let mainNode = initDiscoveryNode(newPrivateKey(), localAddress(20301), @[])
+    for i in 0..<1000:
+        mainNode.addNode(generateNode())
 
-    let peer = initDiscoveryNode(newPrivateKey(), localAddress(20302), @[mainNode.localNode.record])
+    let node = generateNode()
 
-    runForever()
+    let test = await mainNode.lookup(node.id)
+    echo test.len
+    assert test.len > 0
 
 when isMainModule:
-    run()
+    waitFor run()
