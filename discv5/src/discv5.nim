c1c4338979da5f6f3a3c57981c3544636d57281b
decanus <7621705+decanus@users.noreply.github.com>
2020-03-24 21:06:59 +0100

    removed

--- discv5/src/discv5.nim
+++ discv5/src/discv5.nim
@@ -1,36 +1,16 @@
 import
-  confutils, config, random, chronos, sequtils, chronicles, tables, stint, options,
+  random, chronos, sequtils, chronicles, tables, stint, options,
   eth/[keys, rlp], eth/p2p/enode, eth/trie/db,
   eth/p2p/discoveryv5/[discovery_db, enr, node, types, routing_table, encoding],
   eth/p2p/discoveryv5/protocol as discv5_protocol,
   ./utils
 
-proc bootnode() =
+proc run() =
     let mainNode = initDiscoveryNode(newPrivateKey(), localAddress(20301), @[])
 
-    echo mainNode.localNode.record.toURI()
-
-    for i in 0..<1000:
-        mainNode.addNode(generateNode())
-
-proc client(config: DiscConf) =
-
-    var r: Record
-    assert(r.fromURI(config.bootstrap))
-
-    let peer = initDiscoveryNode(newPrivateKey(), localAddress(20302), @[r])
-    peer.start()
-
-proc run(config: DiscConf) =
-    if config.isBootnode:
-        bootnode()
-    else:
-        client(config)
+    let peer = initDiscoveryNode(newPrivateKey(), localAddress(20302), @[mainNode.localNode.record])
 
     runForever()
 
 when isMainModule:
-    let conf = DiscConf.load()
-    run(conf)
-
-
+    run()
