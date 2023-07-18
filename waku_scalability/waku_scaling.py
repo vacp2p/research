40a05e53a03d6df7979a511230088aff8de74a88
0xFugue <119708655+0xFugue@users.noreply.github.com>
2023-07-18 22:18:04 +0530

    typer

--- waku_scalability/waku_scaling.py
+++ waku_scalability/waku_scaling.py
@@ -6,7 +6,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-import typher
+import typer
 
 # Util and format functions
 #-----------------------------------------------------------
